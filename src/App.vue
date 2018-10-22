b9df65baf52ba04400c6cee8283fcda8793ca2d2
tgd <zjrzpp0414@163.com>
2018-10-22 15:36:28 +0800

    添加编辑器配置

--- src/App.vue
+++ src/App.vue
@@ -18,13 +18,13 @@
     color: #2c3e50;
 }
 #nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+	padding: 30px;
+	a {
+		font-weight: bold;
+		color: #2c3e50;
+		&.router-link-exact-active {
+		color: #42b983;
+		}
+	}
 }
 </style>
