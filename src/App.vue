5d8009b1e5ac6aa22e5a677cff3e3c6e1bc843f4
tgd <zjrzpp0414@163.com>
2018-10-22 15:28:06 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -11,11 +11,11 @@
 
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
 }
 #nav {
   padding: 30px;
